5443ea163d01e37c78b3cbb7a0a50444e98d0f15
NightRiders57 <francesco.fittaiolo@gmail.com>
2025-10-16 10:36:43 +0200

    Configure Flask app to use dynamic port
    Added dynamic port configuration for deployment.

--- app.py
+++ app.py
@@ -54,5 +54,7 @@ def invia():
     return render_template('conferma.html', nome=nome, messaggio=messaggio)
 
 if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
-    app.run(debug=True)
