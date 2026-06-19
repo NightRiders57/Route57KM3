11d47160b64418bfb78f87e38ff526d22848a9ff
NightRiders57 <francesco.fittaiolo@gmail.com>
2026-06-19 12:38:14 +0200

    sost calcolo

--- app.py
+++ app.py
@@ -170,22 +170,15 @@ def login():
                 if i.get("whatsapp_accettato")
                 )
             totale_pagamenti = sum(1 for i in iscritti if i.get("pagato"))
-            incasso_totale = sum((int(i.get("passeggeri", 1)) * 42) - (int(i.get("sconti", 0)) * 7) for i in iscritti if i.get("pagato"))
+            incasso_totale = sum(float(i.get("importo_pagato", 0)) for i in iscritti if i.get("pagato"))
             totale_attesa = sum(1 for i in iscritti if not i.get("whatsapp_accettato") and not i.get("whatsapp_rifiutato"))
-            
-            guadagno_totale = 0 
-            for iscrizione in iscritti:
-                if iscrizione.get("pagato"):
-                   passeggeri = int(iscrizione.get("passeggeri", 0))
-                   sconti = int(iscrizione.get("sconti", 0))
-
-                   guadagno = ((passeggeri - sconti)* 10) + (sconti * 7) 
-                   guadagno_totale += guadagno
 
             totale_ristorante = 0
             for iscrizione in iscritti:
                 if iscrizione.get("pagato", False):
                     totale_ristorante += int(iscrizione.get("passeggeri",0)) * 35
+
+            guadagno_totale = incasso_totale - totale_ristorante
 
             return render_template('iscritti.html',iscritti=iscritti, totale=totale, checkin_effettuati=checkin_effettuati, totale_accettati=totale_accettati, totale_rifiutati=totale_rifiutati, totale_passeggeri=totale_passeggeri, totale_pagamenti=totale_pagamenti, incasso_totale=incasso_totale, totale_attesa=totale_attesa, guadagno_totale=guadagno_totale, totale_ristorante=totale_ristorante)
         else:
